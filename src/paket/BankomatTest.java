355efda46cd056894dddae7890ce3cdf4746eb24
Nikola Malinovic <nikolamalinovic97@gmail.com>
2018-08-20 21:53:54 +0200

    Dodao fajlove i BufferedReader

--- src/paket/BankomatTest.java
+++ src/paket/BankomatTest.java
@@ -1,5 +1,10 @@
 package paket;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,6 +12,7 @@ public class BankomatTest {
 
 	public static void main(String[] args) {
 
+		//Deklarisanje (ponegdje i inicijalizacija)
 		Scanner input = new Scanner(System.in);
 		ArrayList<Racun> listaRacuna = new ArrayList<>();
 		int opcija = 0;
@@ -14,7 +20,54 @@ public class BankomatTest {
 		String imeVlasnikaRacuna;
 		double iznosNaRacunu;
 		int sourceAcc, targetAcc;
+		String s;
 		
+		//Deklaracija promjenjljivih potrebnih za input/output
+		File fBrojRacuna = new File("Broj racuna.txt");
+		File fVlasnikRacuna = new File("Vlasnik racuna.txt");
+		File fIznosNaRacunu = new File("Iznos na racunu.txt");
+		BufferedReader brBrojRacuna = null;
+		BufferedReader brVlasnikRacuna = null;
+		BufferedReader brIznosNaRacunu = null;
+		
+		//Kreiranje fajlova, ukoliko vec ne postoje
+		if(! fBrojRacuna.exists())
+			try {
+				fBrojRacuna.createNewFile();
+			} catch (IOException e1) {
+				System.out.println("Greska. File nije kreiran!");
+			}
+		if(! fVlasnikRacuna.exists())
+			try {
+				fVlasnikRacuna.createNewFile();
+			} catch (IOException e1) {
+				System.out.println("Greska. File nije kreiran!");
+			}
+		if(! fIznosNaRacunu.exists())
+			try {
+				fIznosNaRacunu.createNewFile();
+			} catch (IOException e1) {
+				System.out.println("Greska. File nije kreiran!");
+			}
+		
+		//Ucitavanje podataka iz fajlova u array listu
+		try {
+			brBrojRacuna = new BufferedReader(new FileReader(fBrojRacuna));
+			brVlasnikRacuna = new BufferedReader(new FileReader(fVlasnikRacuna));
+			brIznosNaRacunu = new BufferedReader(new FileReader(fIznosNaRacunu));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			while((s = brVlasnikRacuna.readLine()) != null) {
+				listaRacuna.add(new Racun(Integer.parseInt(brBrojRacuna.readLine()), s, Double.parseDouble(brIznosNaRacunu.readLine())));
+			}
+		} catch (IOException e1) {
+			
+		}
+		
+		//Dio programa koji se prikazuje korisniku u konzoli
 		do {
 			
 			System.out.print("\n----------------------MENI----------------------"
